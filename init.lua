a32f459f3bfaee61464b35a4227243f026395bb2
Zach Taylor <zach@objective.dev>
2025-03-04 12:20:32 -0700

    add keymap to save with command-s in insert mode

--- init.lua
+++ init.lua
@@ -189,6 +189,9 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Command-s to save
+vim.keymap.set('i', '<D-s>', '<Esc><cmd>w<CR>', { desc = 'Save' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
