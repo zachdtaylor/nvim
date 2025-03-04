72b262474898c84d3eea5e4c1464e9816b487999
Zach Taylor <zach@objective.dev>
2025-03-04 12:22:02 -0700

    format js/ts files with prettier

--- init.lua
+++ init.lua
@@ -765,7 +765,10 @@ require('lazy').setup({
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
+        javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+        typescript = { 'prettierd', 'prettier', stop_after_first = true },
+        typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
       },
     },
   },
