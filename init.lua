1ba62fc0929ff701518880cc69dcf05ab6f84376
Zach Taylor <zach@objective.dev>
2025-08-13 19:42:12 -0600

    update color scheme

--- init.lua
+++ init.lua
@@ -1005,11 +1005,12 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'rebelot/kanagawa.nvim',
+    'scottmckendry/cyberdream.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
       ---@diagnostic disable-next-line: missing-fields
-      require('kanagawa').setup {
+      require('cyberdream').setup {
+        transparent = true,
         styles = {
           commentStyle = { italic = false }, -- Disable italics in comments
         },
@@ -1018,7 +1019,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'kanagawa'
+      vim.cmd.colorscheme 'cyberdream'
     end,
   },
 
